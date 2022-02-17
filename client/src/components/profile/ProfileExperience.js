94fd0352dc17f8f4dac2249a058ca30cf32d8d17
Amrita tiwari <you@example.com>
2022-02-17 19:07:13 +0530

    added Profile Files

---
+++ client/src/components/profile/ProfileExperience.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const ProfileExperience =  =>{
+    return (
+        <div></div>
+    )
+}
+ProfileExperience.protoType ={
+    profile:PropTypes.object.isRequired
+};
+
+export default ProfileExperience 
