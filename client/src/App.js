9a8f6d9e9941a777422c0064598b8c674ebe7ee4
Amrita tiwari <you@example.com>
2022-02-20 12:33:02 +0530

    adding posts

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
-
-
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';  
 
 //Redux, getting provider from react-redux
@@ -51,10 +50,17 @@ const App = () =>  {
                     <Route exact path='/Profile/:id' component={Profile} />
                     
                     <PrivateRoute exact path='/dashboard' component={Dashboard} />
+
                     <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+
                     <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+
                     <PrivateRoute exact path='/add-experience' component={AddExperience} />
+
                     <PrivateRoute exact path='/add-education' component={AddEducation} />
+
+                    <PrivateRoute exact path='/posts' component={Posts} />
+
                 </Switch>
             </section>
         </Fragment>
