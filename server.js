10a27dfd7d7293fb815c605344516bb106059f91
Amrita Tiwari <amrita20@navgurukul.org>
2022-02-28 23:48:36 +0530

    Prepare-For-Deployment(horoku)

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const app = require('path');
 
 const app= express();
 
@@ -8,14 +9,22 @@ connectDB();
 
 //Init Middleware
 app.use(express.json({ extended: false })); // it allows us to get data from user.js (req.body)
-
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//server static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 const PORT = process.env.PORT || 5000;
 
