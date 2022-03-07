381af6f0cb09369a070982a47d83e35ae2a1a7e1
Amrita Tiwari <amrita20@navgurukul.org>
2022-03-07 15:37:33 +0530

    Prepared for deploment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+
 const app = require('path');
 const app=express();
 
@@ -18,14 +19,14 @@ app.use('/api/posts', require('./routes/api/posts'));
 //server static assets in production
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('client/build'));
+    app. use(express.static('client/build'));
 
-    app.get('*',(req,res)=> {
+    //serve index.html file
+    // create a route
+    app.get('*',(req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
-
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
