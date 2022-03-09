e94a24887405d7428fb664859a82ee7ccf6461fe
Amrita Tiwari <amrita20@navgurukul.org>
2022-03-09 10:21:39 +0530

    changing speling

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -17,7 +17,7 @@ const Posts = ({ getPosts, post: { posts,loading } }) => {
     return loading ? <Spinner /> : <Fragment>
         <h1 className="large text-primary">Posts</h1>
         <p className="lead">
-            <i className="fas fa-user"></i> Welocme to the comunity
+            <i className="fas fa-user"></i>Welcome to the Community
         </p>
         <PostForm />
         <div className="posts">
